2c3266e8cef2bf4299f634336005ca80e5d36591
Sarphu Rongpi <tomonso.ejang@gmail.com>
2023-05-18 19:09:26 +0530

    fix: remove unneccessary files

--- pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import Dashboard from "../src/components/pages/Dashboard";
-
-function HomePage() {
-  return <Dashboard />;
-}
-
-export default HomePage;
--- src/Store/types.d.ts
+++
@@ -1,5 +0,0 @@
-export interface State {
-  darkMode: boolean;
-}
-
-export type Action = { type: "TOGGLE_DARKMODE" };
